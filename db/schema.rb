ba80195b8b6af370bd333ee86f846ac4e7c0793c
EC2 Default User <ec2-user@ip-172-31-94-141.ec2.internal>
2020-04-05 01:44:55 +0000

    Added logo and closed borders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_060132) do
+ActiveRecord::Schema.define(version: 2020_04_04_220704) do
+
+  create_table "rooms", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
