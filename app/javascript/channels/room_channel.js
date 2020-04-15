efe7ccdf586c6c789cb61d06e7af57341e7f848b
EC2 Default User <ec2-user@ip-172-31-94-141.ec2.internal>
2020-04-15 03:50:43 +0000

    added ebextensions and Neil did some css work. adding extensions for aws codeploy and beanstalk

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,43 +1,42 @@
 import consumer from "./consumer"
 
 document.addEventListener('turbolinks:load', function() {
-  consumer.subscriptions.create({
-        channel: "RoomChannel",
-        room_id: getRoomId()
-    }, {
-    connected() {
-        // Called when the subscription is ready for use on the server
-        console.log("Connected!")
-    },
-  
-    disconnected() {
-        // Called when the subscription has been terminated by the server
-    },
-  
-    received(data) {
-        // Called when there's incoming data on the websocket for this channel
-        data = JSON.parse(data.content);
-        console.log(data);
-         YT.get("player").seekTo(data.timestamp);
-        if (data.user_action == "play") {
-            handleVideoPlay();
-        }
-        else if (data.user_action == "pause") {
-            handleVideoPause();
-        }
-        else if (data.user_action == "videoChange") {
-            handleVideoChange(data.video_id);
-        }
-        else if (data.user_action == "chat"){
-            handleChat(data.chat);
-        }
-        else {
-            alert("Invalid Action!");
-        }
-    },
-  
-    broadcast: function() {
-      return this.perform('broadcast');
+    if (document.getElementById('room_id') != null) {
+        consumer.subscriptions.create({
+            channel: "RoomChannel",
+            room_id: getRoomId()
+        }, {
+        connected() {
+            // Called when the subscription is ready for use on the server
+            console.log("Connected!")
+        },
+        disconnected() {
+            // Called when the subscription has been terminated by the server
+        },
+        received(data) {
+            // Called when there's incoming data on the websocket for this channel
+            data = JSON.parse(data.content);
+            console.log(data);
+             YT.get("player").seekTo(data.timestamp);
+            if (data.user_action == "play") {
+                handleVideoPlay();
+            }
+            else if (data.user_action == "pause") {
+                handleVideoPause();
+            }
+            else if (data.user_action == "videoChange") {
+                handleVideoChange(data.video_id);
+            }
+            else if (data.user_action == "chat"){
+                handleChat(data.chat);
+            }
+            else {
+                alert("Invalid Action!");
+            }
+        },
+        broadcast: function() {
+            return this.perform('broadcast');
+            }
+        });
     }
-  });
 });
