fb8eb5c40950e6310f376ad19d058269ade3c349
Neil Marklund <nmarklund10@hotmail.com>
2020-05-17 15:48:51 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get '/video/index'
-  get '/welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # root 'welcome#scene1'
-
+  root 'welcome#redirect'
+  get '/video/index'
+  get '/welcome/index'
   get '/welcome/scene1'
+  get '/welcome/from_outside_controller'
 
   resources :user
   resources :room
