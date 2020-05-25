33d256b7cffe7e6cc6a8412b0ba40ae019a73709
Neil Marklund <nmarklund10@hotmail.com>
2020-05-24 22:33:30 -0400

    added loading

--- app/javascript/packs/createRoom.js
+++ app/javascript/packs/createRoom.js
@@ -1,10 +1,37 @@
-window.createRoom = function(){
+const CREATE = 'create';
+const JOIN = 'join';
+
+const setLoading = function(action, loading) {
+    if (action != CREATE && action != JOIN) {
+        console.error('invalid action!');
+        return;
+    }
+    let content;
+    if (loading) {
+        content = `<div class="spinner-border"></div>`
+    }
+    else {
+        let buttonText = `${action[0].toUpperCase()}${action.slice(1)} Couch`;
+        content =
+            `<div class="input-group-append">
+                <button id="${action}RoomButton" class="btn btn-light" onclick="${action}Room()" type="button">${buttonText}</button>
+            </div>`
+    }
+    let buttonHolderElement = document.getElementById(`${action}RoomButtonHolder`);
+    buttonHolderElement.innerHTML = content;
+}
+
+window.createRoom = function() {
     var username = document.getElementById("username").value.trim();
     if (username != "") {
+        setLoading(CREATE, true);
         makePostRequest('/room', {
             "username": username
         }, function(response) {
             window.location = response.responseText;
+        }, function(error) {
+            setLoading(CREATE, false);
+            console.error(error);
         });
         document.getElementById("username").value = "";
     }
@@ -14,14 +41,15 @@ window.joinRoom = function(){
     var roomId = document.getElementById("roomId").value.trim();
     var user = document.getElementById("user").value.trim();
     if (roomId != "" && user != "") {
+        setLoading(JOIN, true);
         makePostRequest('/room/join', {
             "room_name": roomId,
             "username": user
         }, function(response) {
             window.location = response.responseText;
         }, function(error) {
-            console.log(error);
-            alert(error);
+            setLoading(JOIN, false);
+            console.error(error);
         });
         document.getElementById("user").value = "";
     }
@@ -43,7 +71,6 @@ window.onload = function(){
             document.getElementById("joinRoomButton").click();
         }
     });
-
     //if user presses enter act like they clicked the submit button
     document.getElementById("username").addEventListener('keyup', function (e) {
         if (e.keyCode == 13) {
@@ -51,4 +78,7 @@ window.onload = function(){
             document.getElementById("createRoomButton").click();
         }
     });
+    // Display action buttons on modals
+    setLoading(CREATE, false);
+    setLoading(JOIN, false);
 }
