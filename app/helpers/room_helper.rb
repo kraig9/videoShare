5e061967872a83b52e08227caf8931b7bd0e8928
kraig9 <kraig9@outlook.com>
2020-05-29 23:10:02 -0500

    added method to helper to make room id of size 4 (#43)
    Co-authored-by: Kraig Orcutt <kwo227@tamu.edu>

--- app/helpers/room_helper.rb
+++ app/helpers/room_helper.rb
@@ -1,2 +1,10 @@
 module RoomHelper
+    def generate_random_string(length=4)
+        string = ""
+        chars = ("A".."Z").to_a
+        length.times do
+          string << chars[rand(chars.length-1)]
+        end
+        string
+      end
 end
