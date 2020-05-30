c1f7c101340334a1897e63823dd256eb39bd369b
Kraig Orcutt <kwo227@tamu.edu>
2020-05-30 16:43:14 -0500

    started on the expiration timestamp logic. added column to migration and function stubs to model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,12 @@
 class Room < ApplicationRecord
     has_many :users
+
+    def set_expiration_timestamp
+        
+    end
+
+    def timestamp_Check
+        
+    end
     #attr_accessor :room_name
 end
