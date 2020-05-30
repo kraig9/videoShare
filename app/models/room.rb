aeb04a60195a3ace97ec77d0c26dc93bad7e3b1e
Kraig Orcutt <kwo227@tamu.edu>
2020-05-30 16:55:51 -0500

    added logic to model functions

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,12 +1,17 @@
 class Room < ApplicationRecord
     has_many :users
 
-    def set_expiration_timestamp
-        
+    def set_expiration_timestamp(room_id)
+        #set expiration time to 4 hours after the room creation time
+        Room(id).expiration_time = Room(id).created_at + (3600 * 4)
     end
 
-    def timestamp_Check
-        
+    def timestamp_Check(room_id)
+        if Time.now = Room(id).expiration_time
+            return true
+        else
+            return false
+        end
     end
     #attr_accessor :room_name
 end
