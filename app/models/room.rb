94d2250449c144473cbe379c915ce0b04c71d5df
Kraig Orcutt <kwo227@tamu.edu>
2020-05-30 18:41:15 -0500

    put self in front of methods in model class

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,21 +1,21 @@
 class Room < ApplicationRecord
     has_many :users
 
-    def set_expiration_timestamp(room_id)
+    def self.set_expiration_timestamp(room_id)
         #set expiration time to 4 hours after the room creation time
         Room(id).expiration_time = Room(id).created_at + (3600 * 4)
     end
 
-    def is_timestamp_expired(room_id)
+    def self.is_timestamp_expired(room_id)
         return Time.now >= Room(id).expiration_time ? true : false
     end
 
-    def update_expiration_timestamp(room_id)
+    def self.update_expiration_timestamp(room_id)
         #if there are still users in the room update the expiration timestamp to add another hour
         Room(id).expiration_time + 3600
     end
 
-    def start_check_expiration_thread
+    def self.start_check_expiration_thread
         Thread.new do
             while not is_timestamp_expired do
                 puts "Room: "
