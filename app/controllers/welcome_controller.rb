341c0019613cd5064849d60b1c16d86f13e3466a
Neil Marklund <nmarklund10@hotmail.com>
2020-05-30 17:08:41 -0400

    fixed tabs

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,19 +1,24 @@
 class WelcomeController < ApplicationController
-  def from_outside_controller
-    puts 'OUTSIDE CONTROLLER'
-    if is_authenticated()
-      puts 'rendering room/index'
-      render plain: '/room/index'
-    else
-      puts 'rendering welcome/home'
-      render plain: '/welcome/home'
+    def from_outside_controller
+        puts 'OUTSIDE CONTROLLER'
+        if is_authenticated()
+            puts 'rendering room/index'
+            render plain: '/room/index'
+        else
+            puts 'rendering welcome/home'
+            render plain: '/welcome/home'
+        end
     end
-  end
 
-  def redirect
-    redirect_to '/welcome/home'
-  end
+    def redirect
+        redirect_to '/welcome/home'
+    end
 
-  def scene1
-  end
+    def home
+        if is_authenticated()
+            redirect_to '/room/index'
+        else
+            render '/welcome/home'
+        end
+    end
 end
