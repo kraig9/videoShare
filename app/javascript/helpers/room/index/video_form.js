f23e4b1a742d9be6f20d514c33f6898d71d865f9
Neil Marklund <nmarklund10@hotmail.com>
2020-06-13 12:04:30 -0400

    added enter button handler

--- app/javascript/helpers/room/index/video_form.js
+++ app/javascript/helpers/room/index/video_form.js
@@ -7,16 +7,23 @@ import {
     sendUserLeaving,
 } from './send_server_messages.js';
 
+import {
+    onEnterDoButtonClick
+} from '../../general.js';
+
 export const initializeVideoForm = function() {
     // initialize tooltips
     $(function () {
-        $("#videoLink").tooltip()
+        $('#videoLink').tooltip()
     });
-    document.getElementById("changeVideo")
-            .addEventListener("click", changeVideo);
+    document.getElementById('changeVideo')
+            .addEventListener('click', changeVideo);
 
-    document.getElementById("confrimLeaveYes")
-            .addEventListener("click", sendUserLeaving);
+    document.getElementById('videoLink')
+            .addEventListener('keyup', onEnterDoButtonClick('changeVideo'));
+
+    document.getElementById('confrimLeaveYes')
+            .addEventListener('click', sendUserLeaving);
 }
 
 export const initializeTime = function(event) {
@@ -45,13 +52,14 @@ export const hideError = function() {
 const changeVideo = function(event) {
     const player = YT.get('player');
     let videoUrl = document.getElementById('videoLink').value;
-    let splitUrl = videoUrl.split("=");
+    let splitUrl = videoUrl.split('=');
     // JavaScript has short-circuit evaluation so
     // it will not evalutate the second part of
     // the expression if the first is false
     if (splitUrl.length > 1 && splitUrl[1].length >= 11) {
         let videoId = splitUrl[1].substr(0, 11);
         sendChangeVideo(player, videoId);
+        document.getElementById('videoLink').value = '';
         return;
     }
     displayError('Invalid Youtube URL!')
