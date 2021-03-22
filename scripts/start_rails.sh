278349d39bbbd72701f60c3fbf5f4881edb530b9
Neil Marklund <nmarklund10@hotmail.com>
2021-03-21 23:33:53 -0400

    Add yarn setup

--- scripts/start_rails.sh
+++ scripts/start_rails.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-rm -f tmp/pids/server.pid && bundle exec rails s -b 0.0.0.0
+rm -f tmp/pids/server.pid && yarn install --check-files && rails db:migrate RAILS_ENV=development && bundle exec rails s -b 0.0.0.0
