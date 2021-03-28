f0f19b20fb523f37e4ed2241539ef24d5b851d3f
Neil Marklund <nmarklund10@hotmail.com>
2021-03-28 00:57:17 -0400

    Add pg gem

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_04_06_015737) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "rooms", force: :cascade do |t|
     t.string "room_name"
     t.datetime "created_at", precision: 6, null: false
