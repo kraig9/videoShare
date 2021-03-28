7003b99f4b9183d80a8420e730b875bb62ad2601
Henry Stephens <henry@dopeHypervisor>
2021-03-28 11:28:25 -0400

    remove RAILS_ENV

--- scripts/start_rails.sh
+++ scripts/start_rails.sh
@@ -3,5 +3,5 @@
 rm -f tmp/pids/server.pid && \
   yarn install --check-files && \
   rails db:setup && \
-  rails db:migrate RAILS_ENV=development && \
+  rails db:migrate && \
   bundle exec rails s -b 0.0.0.0
