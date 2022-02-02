0d155718d17781579620654aa1b0c4a19fc09952
user <user>
2022-02-02 17:32:21 -0500

    update config files to rails 7 format

---
+++ config/importmap.rb
@@ -0,0 +1,5 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "react", to: "https://cdn.jsdelivr.net/npm/react@17.0.2/index.js"
+pin "react-dom", to: "https://cdn.jsdelivr.net/npm/react-dom@17.0.2/index.js"
